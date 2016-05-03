5eb8d91aade802ca0070883206f46110d8a90c43
Mihaly Vadai <vadaim@gmail.com>
2016-05-03 15:59:28 +0100

    Datalogging and export functions added

--- LOG.py
+++ LOG.py
@@ -4,6 +4,7 @@
 # (c) 2016 Mihaly Vadai http://muonhunter.com
 #
 # Licence: GPL v.3
+# see LICENCE
 
 avr_address = 0x19
 
@@ -35,38 +36,37 @@ if logging_time < 1:
 	print('Continuing for 60 seconds.')
 	logging_time = 60
 
-def update_db():
-	pass
-	
-def print_output(gm1,gm2,muon):
-	sys.stderr.write("\x1b[2J\x1b[H")
-	print 'Muon Hunter datalogging\n'.center(72)
-	print '\nDatalogging started: {0}\n'.format(start_time)
-	print '{0:9}{1:15}'.format('Signal','Total counts')
-	print '{0:9}{1:12d}\n'.format('GM1',gm1)
-	if GM2 != 0:
-		print '{0:9}{1:12d}\n'.format('GM2',gm2)
-	if COSMIC != 0:
-		print '{0:9}{1:15}'.format('Signal','Total counts')
-		print '{0:9}{1:12d}\n'.format('COSMIC',muon)
-	print 'Temperature = {0:.1f} *C'.format(BMP180.TEMP)
-	print 'Pressure = {0:d} Pa'.format(int(BMP180.PRESSURE))
-	print 'Time remaining: {0:d} s\n'.format(int(round(logging_time - (time() - start_time))))
-	print '(c) 2016 Mihaly Vadai'
-	print 'For more information visit:\nhttp://muonhunter.com'
-
 conn = sqlite3.connect(database)
 c = conn.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS runs (runid INTEGER PRIMARY KEY AUTOINCREMENT, 
 	starttime TIMESTAMP DEFAULT CURRENT_TIMESTAMP) ''')
 c.execute('''CREATE TABLE IF NOT EXISTS run (run INTEGER, 
-	channel TEXT, hitid INTEGER, time REAL, pressure REAL, temperature REAL,
-	coincidence_time INTEGER, confirmed_flag INTEGER, image_flag INTEGER,
+	channel TEXT, time TIMESTAMP DEFAULT CURRENT_TIMESTAMP, pressure REAL, temperature REAL,
+	total_counts INTEGER, counts_per_period INTEGER, exrap_flag INTEGER, image_flag INTEGER,
 	image_name TEXT, FOREIGN KEY(run) REFERENCES runs(runid))''')
 	
 c.execute('''INSERT INTO runs DEFAULT VALUES''')
 conn.commit()
 runid = c.execute('''SELECT * FROM runs ORDER BY runid DESC LIMIT 1''').fetchone()[0]
+
+def print_output(*args):
+	sys.stderr.write("\x1b[2J\x1b[H")
+	print 'Muon Hunter datalogging\n'.center(72)
+	print '\nDatalogging started: {0}       Run ID = {1:d} \n'.format(start_time, int(runid))
+	print '{0:9}{1:15}{2:16}{3:12}'.format('Signal','Total counts', 'Hits per min', 'Extrapolated')
+	print '{0:9}{1:12d}{2:15d} {3:12d}\n'.format('GM1',gm1_total,gm1_per_min,gm1_ex_flag)
+	print '{0:9}{1:12d}{2:15d} {3:12d}\n'.format('GM2',gm2_total,gm2_per_min,gm2_ex_flag)
+	print '{0:9}{1:15}{2:17}{3:12}'.format('Signal','Total counts', 'Hits per hour', 'Extrapolated')
+	print '{0:9}{1:12d}{2:15d} {3:12d}\n'.format('COSMIC',muon_total,muon_per_hour,muon_ex_flag)
+	print 'Temperature = {0:.1f} *C'.format(BMP180.TEMP)
+	print 'Pressure = {0:d} Pa'.format(int(BMP180.PRESSURE))
+	print 'Time remaining: {0:d} s\n'.format(int(round(logging_time - (time() - start_time))))
+	print 'To abort press CTRL + C. Data is saved every {} second.\n'.format(refresh_rate)
+	print 'For more information visit:\nhttp://muonhunter.com'
+
+
+
+
 
 start_time = time()
 BMP180 = PAC(runid, start_time)
@@ -82,11 +82,28 @@ while time() < start_time + logging_time:
 		except IOError:
 			print 'I2C Bus still busy, exiting.'
 			raise
-		
 	muon_total = data[1] + (data[2] << 8)
 	gm2_total = data[3] + (data[4] << 8) + (data[5] << 16) + (data[6] << 24)
 	gm1_total = data[7] + (data[8] << 8) + (data[9] << 16) + (data[10] << 24)
-	update_db()
-	print_output(gm1_total,gm2_total,muon_total)
-	sleep(refresh_rate)
+	muon_per_hour = data[11] + (data[12] << 8)
+	muon_ex_flag = data[13]
+	gm2_per_min = data[14] + (data[15] << 8)
+	gm2_ex_flag = data[16]
+	gm1_per_min = data[17] + (data[18] << 8)
+	gm1_ex_flag = data[19]
+	
 	BMP180.update()
+	
+	print_output(muon_total, gm2_total, gm1_total, muon_per_hour,
+		muon_ex_flag, gm2_per_min, gm2_ex_flag, gm1_per_min, gm1_ex_flag)
+		
+	gm1_data = (runid, 'GM1', BMP180.PRESSURE, BMP180.TEMP, gm1_total, gm1_per_min, gm1_ex_flag, 0, '')
+	gm2_data = (runid, 'GM2', BMP180.PRESSURE, BMP180.TEMP, gm2_total, gm2_per_min, gm2_ex_flag, 0, '')
+	muon_data = (runid, 'Muon', BMP180.PRESSURE, BMP180.TEMP, muon_total, muon_per_hour, muon_ex_flag, 0, '')
+	c.execute('INSERT INTO run VALUES (?, ?, DateTime("now"), ?, ?, ?, ?, ?, ?, ?)', gm1_data)
+	c.execute('INSERT INTO run VALUES (?, ?, DateTime("now"), ?, ?, ?, ?, ?, ?, ?)', gm2_data)
+	c.execute('INSERT INTO run VALUES (?, ?, DateTime("now"), ?, ?, ?, ?, ?, ?, ?)', muon_data)
+	conn.commit()
+	
+	sleep(refresh_rate)
+	
---
+++ export.py
@@ -0,0 +1,70 @@
+# export.py
+# Created: 18/08/2015
+# Last modified: 03/05/2016
+# 
+# Author: Mihaly Vadai
+# Website:	http://muonhunter.com
+# 
+# For credits see README.md
+#
+# License: GPL v.3
+# Version: 0.3
+
+from optparse import OptionParser
+import sqlite3
+import csv
+from time import time, gmtime, strftime
+import sys
+
+database = 'muonhunter.db'
+conn = sqlite3.connect(database)
+c = conn.cursor()
+
+parser = OptionParser()
+parser.add_option("-f", "--file", dest="filename",
+	help="write report to this file", metavar="FILE")
+parser.add_option("-r", "--run", dest="run", help="run id")
+parser.add_option("-c", "--channel", dest="channel", help="GM1 or GM2 or muon")
+
+args = parser.parse_args()[0]
+filename = args.filename
+run = args.run
+
+runs = []
+
+for row in c.execute("SELECT * from runs"):
+	runs.append(row[0])
+	
+if run != None and int(run) not in runs:
+	print "Run doesn't exist, try one of these:" 
+	for row in runs:
+		print row
+	sys.exit("Exiting: 1")
+
+channel = args.channel
+
+if channel == None or channel == 'GM1' or channel == 'GM2' or channel == 'muon':
+	pass
+else:
+	print "Not valid channel ID. Try: GM1, GM2 or muon" 
+	sys.exit("Exiting: 1")
+
+	
+if filename == None:
+	filename = 'muonhunter-%s.csv' % (strftime("%d-%b-%Y-%H-%M-%S", gmtime()))
+
+with open(filename, 'wb') as csvfile:
+	gmwriter = csv.writer(csvfile, delimiter=',')
+	gmwriter.writerow(("RUN ID", "Channel", "Log time", "Pressure (Pa)", "Temperature *C", "Total hits", "Counts per period", "Image flag", "Image name"))
+	if run == None and channel == None:
+		for row in c.execute("SELECT * from run"):
+			gmwriter.writerow(row)
+	elif run == None:
+		for row in c.execute("SELECT * from run WHERE channel = ?", (channel,)):
+        	        gmwriter.writerow(row)
+	elif channel == None:
+                for row in c.execute("SELECT * from run WHERE run = ?", (run,)):
+                	gmwriter.writerow(row)
+	else:
+                for row in c.execute("SELECT * from run WHERE run = ? AND channel = ?", (run,channel)):
+                	gmwriter.writerow(row)
